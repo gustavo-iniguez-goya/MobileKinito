5ee37c2e0eaeac2b430d4dece2d89b13613accbe
Gustavo Iñiguez Goya <gustavo-iniguez-goya@users.noreply.github.com>
2006-02-10 17:58:56 +0000

    Added the properly license

--- src/Kinito.java
+++ src/Kinito.java
@@ -1,4 +1,23 @@
 
+/* Kinito for mobile phones - The first Kinito game for Mobile Phones
+ * Copyright (C) 2005-2006 - Gustavo Iñiguez Goya
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
 package Kinito;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.lcdui.CommandListener;
